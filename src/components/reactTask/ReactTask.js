90385e7acb77a0e698c7bb21eea5d96b624bb9d9
unknown <teqa@wp.pl>
2021-12-08 00:30:43 +0100

    Changes

--- src/components/reactTask/ReactTask.js
+++ src/components/reactTask/ReactTask.js
@@ -5,50 +5,46 @@ import Navbar from "../Navbar";
 
 const ReactTask = () => {
   const [quotes, setQuotes] = useState([]);
-  const randomNumberOfQuote = Math.floor(Math.random() * quotes.length);
-  const [myQuote, setMyQuote] = useState({ quote: "", author: "" });
+  const [currentQuote, setCurrentQuote] = useState({ quote: "", author: "" });
+  const [quoteList, setQuoteList] = useState([]);
+  let idx = quoteList.length;
 
-  const getQuotes = async () => {
-    await axios
+  const newQuote = () => {
+    const randomNumberOfQuote = Math.floor(Math.random() * quotes.length);
+    setCurrentQuote(quotes[randomNumberOfQuote]);
+    setQuoteList([...quoteList, currentQuote]);
+    idx += 1;
+  };
+
+  useEffect(() => {
+    axios
       .get(
         "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
       )
       .then((res) => setQuotes(res.data));
-  };
-
-  useEffect(() => {
-    getQuotes();
   }, []);
 
-  const setNewQuote = () => {
-    setMyQuote(quotes[randomNumberOfQuote]);
-    console.log(myQuote);
-  };
-
   const showPrevQuote = () => {
-    setMyQuote((prevState) => ({
-      quote: prevState.quote,
-      author: prevState.author,
-    }));
+    setQuoteList((names) => names.filter((_, i) => i !== names.length - 1));
+    idx = quoteList.length - 1;
+    setCurrentQuote({
+      quote: quoteList[idx].quote,
+      author: quoteList[idx].author,
+    });
   };
-  console.log(myQuote);
 
   return (
     <div>
       <Navbar />
       <div>
-        <h1 className="quote">
-          "{quotes.length ? quotes[randomNumberOfQuote].quote : null} "
-        </h1>
-        <h1>
-          Author: {quotes.length ? quotes[randomNumberOfQuote].author : null}
-        </h1>
+        <h1 className="quote">"{currentQuote && currentQuote.quote}"</h1>
+        <h1>Author: {currentQuote && currentQuote.author}</h1>
       </div>
 
-      <button disabled={!myQuote} onClick={showPrevQuote}>
+      <button disabled={idx <= 0} onClick={showPrevQuote}>
         Previous quote
       </button>
-      <button onClick={setNewQuote}>Get new quote</button>
+      <button onClick={newQuote}>Get new quote</button>
     </div>
   );
 };
